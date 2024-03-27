553a533eb5ca4f5edd384182698f630a0af557e9
David <127497720+davidetuazon@users.noreply.github.com>
2024-03-27 13:28:49 +0800

    updated order page

--- Ordering/orderscript.js
+++ Ordering/orderscript.js
@@ -8,99 +8,105 @@ const main = document.querySelector('main');
 const total = document.querySelector('.total');
 const quantity = document.querySelector('.quantity');
 
-const drinks = [
+const products = [
     {
         id: 1,
         name: 'Iced Americano',
         image: 'IcedAmericano.png',
-        price: 99.00
+        price: 99.00,
+        category: "beverage"
     },
     {
         id: 2,
         name: 'Iced Caramel Macchiato',
         image: 'icedCaramelMacchiato.png',
-        price: 125.00
+        price: 125.00,
+        category: "beverage"
     },
     {
         id: 3,
         name: 'Iced Latte',
         image: 'icedLatte.png',
-        price: 110.00
+        price: 110.00,
+        category: "beverage"
     }, 
     {
         id: 4,
         name: 'Iced Matcha Latte',
         image: 'icedMatchaLatte.png',
-        price: 125.00
+        price: 125.00,
+        category: "beverage"
     },
     {
         id: 5,
         name: 'Iced Mocha',
         image: 'icedMocha.png',
-        price: 120.00
-    }
-]
-
-const pastries = [
+        price: 120.00,
+        category: "beverage"
+    },
     {
-        id: 1,
+        id: 6,
         name: "Butter Croissant",
         image: "ButterCroissant.png",
-        price: 105.00
+        price: 105.00,
+        category: "pastry"
     },
     {
-        id: 2,
+        id: 7,
         name: "Blueberry Bagel",
         image: "BlueberryBagel.png",
-        price: 135.00
+        price: 135.00,
+        category: "pastry"
     },
     {
-        id: 3,
+        id: 8,
         name: "Cheese Bacon Danish",
         image: "CheeseBaconDanish.png",
-        price: 210.00
+        price: 210.00,
+        category: "pastry"
     },
     {
-        id: 4,
+        id: 9,
         name: "Blueberry Muffin",
         image: "BlueberryMuffin.png",
-        price: 165.00
+        price: 165.00,
+        category: "pastry"
     },
     {
-        id: 5,
+        id: 10,
         name: "Waffle",
         image: "Waffle.png",
-        price: 105.00
+        price: 105.00,
+        category: "pastry"
     }
 ]
 
 
 const displayProducts = () => {
-    drinks.forEach((value, key) => {
-        let newDiv = document.createElement('div');
-        newDiv.classList.add('beverage');
-        newDiv.innerHTML = `
-        <img src='../Ordering/Pictures/${value.image}'/>
-        <div class='title'>${value.name}</div>
-        <div class='price'>Php ${value.price.toLocaleString()}</div>
-        <button onclick='addToCart(${key})'>Add to cart</button>
-        `;
-        drinkList.appendChild(newDiv);
-    })
-
-    pastries.forEach((value, key) => {
-        let newDiv = document.createElement('div');
-        newDiv.classList.add('pastry');
-        newDiv.innerHTML = `
-        <img src='../Ordering/Pictures/${value.image}'/>
-        <div class='title'>${value.name}</div>
-        <div class='price'>Php ${value.price.toLocaleString()}</div>
-        <button onclick='addToCart(${key})'>Add to cart</button>
-        `;
-        pastryList.appendChild(newDiv);
+    products.forEach((value) => {
+        if(value.category === "beverage") {
+            let newDiv = document.createElement('div');
+            newDiv.classList.add('beverage');
+            newDiv.innerHTML = `
+            <img src='../Ordering/Pictures/${value.image}'/>
+            <div class='title'>${value.name}</div>
+            <div class='price'>Php ${value.price.toLocaleString()}</div>
+            <button class='btn add-to-cart-btn'>Add to cart</button>
+            `;
+            drinkList.appendChild(newDiv);
+        } else {
+            let newDiv = document.createElement('div');
+            newDiv.classList.add('pastry');
+            newDiv.innerHTML = `
+            <img src='../Ordering/Pictures/${value.image}'/>
+            <div class='title'>${value.name}</div>
+            <div class='price'>Php ${value.price.toLocaleString()}</div>
+            <button class='btn add-to-cart-btn'>Add to cart</button>
+            `;
+            pastryList.appendChild(newDiv);
+        }
     })
 }
-
 displayProducts();
 
 openCart.addEventListener('click', () => {
@@ -109,3 +115,17 @@ openCart.addEventListener('click', () => {
     cartContainer.style.display = "none";
 });
 
+class ShoppingCart {
+    constructor() {
+        this.items = [];
+        this.total = 0;
+        this.count = 0;
+    }
+
+    addItem(id, products) {
+
+    }
+}
+
+
+const cart = new ShoppingCart();
